d0f625d9267edab4855b0a052087cdd957494082
Julien Bernard <jbernard@c2r1p3.42quebec.com>
2021-10-20 15:50:27 -0400

    First push

---
+++ main.c
@@ -0,0 +1,6 @@
+ 
+#include<stdio.h> 
+int main(){
+	printf("This is printed in the stdout :) Allez on avance"); 
+	return (0); 
+}
---
+++ main.py
@@ -0,0 +1,24 @@
+import sys 
+import subprocess 
+import os 
+
+EXEC_FILENAME = "a.out"
+C_FILENAME = ["main.c"]
+GCC = [
+	"gcc",
+	"-Wall",
+	"-Werror",
+	"-Wextra",
+] + C_FILENAME
+
+def compile():
+	global GCC
+	subprocess.call(GCC)
+
+def run_exec():
+	global EXEC_FILENAME
+	subprocess.call(["./" + EXEC_FILENAME]) 
+
+if __name__ == "__main__":
+	compile()
+	run_exec()
