ff4d61a476e06d1f26a30a22d540c681ecd42fe6
tonTed <yddetco@gmail.com>
2021-10-30 00:12:42 -0400

    test split str and int

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import printf_test
 
 if __name__ == "__main__":
 	return_ = printf_test.PrintfTest("printf", "Allo %c %d", 'q', 42)
-	return_
+	split = str(return_).split("::")
+	split[1] = int(split[1])
+	print(split)
